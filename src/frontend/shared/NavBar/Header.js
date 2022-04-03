a9bff4d16764c48f4171461c03eec0ade2156f4e
Om Kakatkar <omkakatkar@gmail.com>
2022-04-03 19:56:14 +0530

    [FEAT] Add Sidebar and Header styles

---
+++ src/frontend/shared/NavBar/Header.js
@@ -0,0 +1,6 @@
+import './Header.css';
+function Header() {
+	return <div className="header text-huge">Header</div>;
+}
+
+export default Header;
---
+++ src/frontend/shared/SideBar/SideBar.js
@@ -0,0 +1,35 @@
+import './SideBar.css';
+function SideBar() {
+	return (
+		<div className="sidebar">
+			<div className="sidebar-spacer"></div>
+			<div className="sidebar-content">
+				<div className="sidebar-inner-content">
+					{/* FIXME: Add routing and actual content */}
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+					<div>Sidebar</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default SideBar;
