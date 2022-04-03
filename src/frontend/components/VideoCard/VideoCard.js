923fec5d82b2985210688cf544061a078ae427be
Om Kakatkar <omkakatkar@gmail.com>
2022-04-03 22:32:04 +0530

    [FEAT] Remove react player, add image tag

--- src/frontend/components/VideoCard/VideoCard.js
+++ src/frontend/components/VideoCard/VideoCard.js
@@ -1,29 +1,25 @@
-import ReactPlayer from 'react-player/youtube';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
-import { getImageUrl, getVideoUrl } from '../../utils/video-helpers';
+import { Link } from 'react-router-dom';
+import { getImageUrl } from '../../utils/video-helpers';
 import './VideoCard.css';
 
 export function VideoCard({ videoData }) {
 	const { _id, title, category } = videoData;
 	return (
 		<article className="video-card">
-			<div className="card-media">
-				<ReactPlayer
-					url={getVideoUrl(_id)}
-					light={getImageUrl(_id)}
-					controls
-					width="100%"
-					height="100%"
-				/>
-			</div>
-			<div className="video-card-body">
-				<h3 className="text-md text-white card-heading">{title}</h3>
-				<h5 className="text-sm text-gray">{category}</h5>
-			</div>
-			<button className="card-btn text-lg">
-				<FontAwesomeIcon icon={faEllipsisVertical} />
-			</button>
+			<Link to={`/watch/${category}/${_id}`}>
+				<div className="card-media">
+					<img src={getImageUrl(_id)} alt={title} />
+				</div>
+				<div className="video-card-body">
+					<h3 className="text-md text-white card-heading">{title}</h3>
+					<h5 className="text-sm text-gray">{category}</h5>
+				</div>
+				<button className="card-btn text-lg">
+					<FontAwesomeIcon icon={faEllipsisVertical} />
+				</button>
+			</Link>
 		</article>
 	);
 }
