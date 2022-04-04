d36046e7f6de25b276103370a533029a41774d49
Om Kakatkar <omkakatkar@gmail.com>
2022-04-04 16:34:55 +0530

    [FEAT] Add styles for Header and Sidebar with link

--- src/frontend/shared/NavBar/Header.js
+++ src/frontend/shared/NavBar/Header.js
@@ -1,6 +1,30 @@
+import { faBars, faSearch, faTelevision } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Header.css';
 function Header() {
-	return <div className="header text-huge">Header</div>;
+	return (
+		<header className="header flex">
+			<div className="flex">
+				<FontAwesomeIcon icon={faBars} className="header-icon text-xlg" />
+				<div className="flex header-brand">
+					<FontAwesomeIcon
+						icon={faTelevision}
+						className="header-logo text-huge"
+					/>
+					<h2 className="header-text text-xlg">ANI WATCH</h2>
+				</div>
+			</div>
+			<div className='header-searchbar-wrapper'>
+				<input type="search" className='header-searchbar text-white'/>
+        <button className='btn'>
+          <FontAwesomeIcon icon={faSearch} className='text-white text-lg'/>
+        </button>
+			</div>
+			<div className="avatar flex-container">
+				<div className="avatar-content text-white font-bold">R</div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
